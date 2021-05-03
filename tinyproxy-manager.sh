8a77834aa17f8bffc2375965171b09124d2b2b90
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-05-03 09:37:25 -0400

    updated var names

--- tinyproxy-manager.sh
+++ tinyproxy-manager.sh
@@ -25,22 +25,22 @@ dist-check
 
 # Pre-Checks system requirements
 function installing-system-requirements() {
-  if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ] || [ "$DISTRO" == "linuxmint" ] || [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ] || [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ] || [ "$DISTRO" == "alpine" ] || [ "$DISTRO" == "freebsd" ]; }; then
+  if { [ "${DISTRO}" == "ubuntu" ] || [ "${DISTRO}" == "debian" ] || [ "${DISTRO}" == "raspbian" ] || [ "${DISTRO}" == "pop" ] || [ "${DISTRO}" == "kali" ] || [ "${DISTRO}" == "linuxmint" ] || [ "${DISTRO}" == "fedora" ] || [ "${DISTRO}" == "centos" ] || [ "${DISTRO}" == "rhel" ] || [ "${DISTRO}" == "arch" ] || [ "${DISTRO}" == "manjaro" ] || [ "${DISTRO}" == "alpine" ] || [ "${DISTRO}" == "freebsd" ]; }; then
     if { [ ! -x "$(command -v curl)" ]; }; then
-      if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ] || [ "$DISTRO" == "linuxmint" ]; }; then
+      if { [ "${DISTRO}" == "ubuntu" ] || [ "${DISTRO}" == "debian" ] || [ "${DISTRO}" == "raspbian" ] || [ "${DISTRO}" == "pop" ] || [ "${DISTRO}" == "kali" ] || [ "${DISTRO}" == "linuxmint" ]; }; then
         apt-get update && apt-get install curl -y
-      elif { [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ]; }; then
+      elif { [ "${DISTRO}" == "fedora" ] || [ "${DISTRO}" == "centos" ] || [ "${DISTRO}" == "rhel" ]; }; then
         yum update -y && yum install curl -y
-      elif { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ]; }; then
+      elif { [ "${DISTRO}" == "arch" ] || [ "${DISTRO}" == "manjaro" ]; }; then
         pacman -Syu --noconfirm curl
-      elif [ "$DISTRO" == "alpine" ]; then
+      elif [ "${DISTRO}" == "alpine" ]; then
         apk update && apk add curl
-      elif [ "$DISTRO" == "freebsd" ]; then
+      elif [ "${DISTRO}" == "freebsd" ]; then
         pkg update && pkg install curl
       fi
     fi
   else
-    echo "Error: $DISTRO not supported."
+    echo "Error: ${DISTRO} not supported."
     exit
   fi
 }
@@ -53,17 +53,17 @@ TINYPROXY_CONFIG="/etc/tinyproxy/tinyproxy.conf"
 TINYPROXY_MANGER="/etc/tinyproxy/tinyproxy-manager"
 TINYPROXY_MANAGER_UPDATE="https://raw.githubusercontent.com/complexorganizations/tinyproxy-manager/main/tinyproxy-manager.sh"
 
-if [ ! -f "$TINYPROXY_CONFIG" ]; then
+if [ ! -f "${TINYPROXY_CONFIG}" ]; then
 
   # Ask the user for their ip
   function server-input-ip() {
     echo "What IP would u like the server to take allow?"
     echo "  1) Custom (Recommended)"
     echo "  2) Ansewer #2 (Everything)"
-    until [[ "$FIRST_QUESTION_SETTINGS" =~ ^[1-2]$ ]]; do
+    until [[ "${FIRST_QUESTION_SETTINGS}" =~ ^[1-2]$ ]]; do
       read -rp "Subnetwork choice [1-2]: " -e -i 1 FIRST_QUESTION_SETTINGS
     done
-    case $FIRST_QUESTION_SETTINGS in
+    case ${FIRST_QUESTION_SETTINGS} in
     1)
       read -rp "User text: " -e -i "" FIRST_QUESTION
       ;;
@@ -81,10 +81,10 @@ if [ ! -f "$TINYPROXY_CONFIG" ]; then
     echo "What port would u like the server to take allow?"
     echo "  1) 8080 (Recommended)"
     echo "  2) Custom (Everything)"
-    until [[ "$SECOND_QUESTION_SETTINGS" =~ ^[1-2]$ ]]; do
+    until [[ "${SECOND_QUESTION_SETTINGS}" =~ ^[1-2]$ ]]; do
       read -rp "Subnetwork choice [1-2]: " -e -i 1 SECOND_QUESTION_SETTINGS
     done
-    case $SECOND_QUESTION_SETTINGS in
+    case ${SECOND_QUESTION_SETTINGS} in
     1)
       SECOND_QUESTION="8080"
       ;;
@@ -100,19 +100,19 @@ if [ ! -f "$TINYPROXY_CONFIG" ]; then
   # Pre-Checks system requirements
   function install-the-app() {
     if { [ ! -x "$(command -v tinyproxy)" ]; }; then
-      if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ] || [ "$DISTRO" == "linuxmint" ]; }; then
+      if { [ "${DISTRO}" == "ubuntu" ] || [ "${DISTRO}" == "debian" ] || [ "${DISTRO}" == "raspbian" ] || [ "${DISTRO}" == "pop" ] || [ "${DISTRO}" == "kali" ] || [ "${DISTRO}" == "linuxmint" ]; }; then
         apt-get update
         apt-get install tinyproxy -y
-      elif { [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ]; }; then
+      elif { [ "${DISTRO}" == "fedora" ] || [ "${DISTRO}" == "centos" ] || [ "${DISTRO}" == "rhel" ]; }; then
         yum update -y
         yum install tinyproxy -y
-      elif { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ]; }; then
+      elif { [ "${DISTRO}" == "arch" ] || [ "${DISTRO}" == "manjaro" ]; }; then
         pacman -Syu
         pacman -Syu --noconfirm tinyproxy
-      elif [ "$DISTRO" == "alpine" ]; then
+      elif [ "${DISTRO}" == "alpine" ]; then
         apk update
         apk add tinyproxy
-      elif [ "$DISTRO" == "freebsd" ]; then
+      elif [ "${DISTRO}" == "freebsd" ]; then
         pkg update
         pkg install tinyproxy
       fi
@@ -123,9 +123,9 @@ if [ ! -f "$TINYPROXY_CONFIG" ]; then
   install-the-app
 
   function service-manager() {
-    if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ] || [ "$DISTRO" == "linuxmint" ] || [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ] || [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ] || [ "$DISTRO" == "alpine" ] || [ "$DISTRO" == "freebsd" ]; }; then
-      sed -i "s|Allow 127.0.0.1|Allow $FIRST_QUESTION|" $TINYPROXY_CONFIG
-      sed -i "s|Port 8888|Port $SECOND_QUESTION|" $TINYPROXY_CONFIG
+    if { [ "${DISTRO}" == "ubuntu" ] || [ "${DISTRO}" == "debian" ] || [ "${DISTRO}" == "raspbian" ] || [ "${DISTRO}" == "pop" ] || [ "${DISTRO}" == "kali" ] || [ "${DISTRO}" == "linuxmint" ] || [ "${DISTRO}" == "fedora" ] || [ "${DISTRO}" == "centos" ] || [ "${DISTRO}" == "rhel" ] || [ "${DISTRO}" == "arch" ] || [ "${DISTRO}" == "manjaro" ] || [ "${DISTRO}" == "alpine" ] || [ "${DISTRO}" == "freebsd" ]; }; then
+      sed -i "s|Allow 127.0.0.1|Allow ${FIRST_QUESTION}|" ${TINYPROXY_CONFIG}
+      sed -i "s|Port 8888|Port ${SECOND_QUESTION}|" ${TINYPROXY_CONFIG}
     elif pgrep systemd-journal; then
       systemctl enable tinyproxy
       systemctl restart tinyproxy
@@ -139,8 +139,8 @@ if [ ! -f "$TINYPROXY_CONFIG" ]; then
   service-manager
 
   function install-tiny-proxy-manager() {
-    if [ ! -f "$TINYPROXY_MANGER" ]; then
-      echo "TinyProxy: true" >>$TINYPROXY_MANGER
+    if [ ! -f "${TINYPROXY_MANGER}" ]; then
+      echo "TinyProxy: true" >>${TINYPROXY_MANGER}
     fi
   }
 
@@ -158,27 +158,27 @@ else
     done
     case $USER_OPTIONS in
     1)
-      rm -f $TINYPROXY_MANGER
-      rm -f $TINYPROXY_CONFIG
-      if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ] || [ "$DISTRO" == "linuxmint" ]; }; then
+      rm -f ${TINYPROXY_MANGER}
+      rm -f ${TINYPROXY_CONFIG}
+      if { [ "${DISTRO}" == "ubuntu" ] || [ "${DISTRO}" == "debian" ] || [ "${DISTRO}" == "raspbian" ] || [ "${DISTRO}" == "pop" ] || [ "${DISTRO}" == "kali" ] || [ "${DISTRO}" == "linuxmint" ]; }; then
         apt-get update && apt-get remove --purge tinyproxy -y
-      elif { [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ]; }; then
+      elif { [ "${DISTRO}" == "fedora" ] || [ "${DISTRO}" == "centos" ] || [ "${DISTRO}" == "rhel" ]; }; then
         yum update -y && yum remove tinyproxy -y
-      elif { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ]; }; then
+      elif { [ "${DISTRO}" == "arch" ] || [ "${DISTRO}" == "manjaro" ]; }; then
         pacman -Syu --noconfirm tinyproxy
-      elif [ "$DISTRO" == "alpine" ]; then
+      elif [ "${DISTRO}" == "alpine" ]; then
         apk update
         apk add tinyproxy
-      elif [ "$DISTRO" == "freebsd" ]; then
+      elif [ "${DISTRO}" == "freebsd" ]; then
         pkg update
         pkg install tinyproxy
       fi
       ;;
     2)
       CURRENT_FILE_PATH="$(realpath "$0")"
-      if [ -f "$CURRENT_FILE_PATH" ]; then
-        curl -o "$CURRENT_FILE_PATH" $TINYPROXY_MANAGER_UPDATE
-        chmod +x "$CURRENT_FILE_PATH" || exit
+      if [ -f "${CURRENT_FILE_PATH}" ]; then
+        curl -o "${CURRENT_FILE_PATH}" ${TINYPROXY_MANAGER_UPDATE}
+        chmod +x "${CURRENT_FILE_PATH}" || exit
       fi
       ;;
     esac
